b204525ac5f14e79aa242f2c050d15496fc5872a
Maxime Vidori <maxime.vidori@gmail.com>
2017-04-25 11:14:56 +0200

    Use docker-compose now

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker build -t habitat --build-arg uid=$(id -u) .
--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker run --rm --name=habitat --tty --interactive --volume=$(pwd):/mnt \
-    --cap-add=SYS_ADMIN --user=$(id -u):$(id -g) habitat sh
